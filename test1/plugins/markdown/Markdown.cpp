9f36498830bc74e07a393867bd7c03ad78393487
Sebastian Karlsson] <sebbekarlsson97@gmail.com>
2016-08-09 01:56:03 +0200

    removed markdown stuff

--- test1/plugins/markdown/Markdown.cpp
+++
@@ -1,5 +0,0 @@
-#include "Markdown.h"
-
-std::string Markdown::process(std::string html) {
-    return html;
-}
--- test1/plugins/markdown/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Markdown.h"
-
-
-int main(int argc, char* argv[]) {
-    std::cout << Markdown::process(std::string("Hello There")) << std::endl;
-    return 0;
-} 
