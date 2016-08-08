abc532538e08b86860b7134bc1b5a6105902634b
Sebastian Karlsson] <sebbekarlsson97@gmail.com>
2016-08-08 23:01:21 +0200

    purged compiler class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-#include "WebsiteCompiler.h"
 #include "Website.h"
 #include <string>
 
 
 int main(int argc, char* argv[]) {
-    WebsiteCompiler::compile(new Website(std::string(argv[1])));
+    Website * website = new Website(std::string(argv[1]));
+
+    website->addHeaderElement(
+            "<link rel='stylesheet' type='text/css' href='style.css'>"
+            );
+    website->addHeaderElement(
+            "<link rel='stylesheet' type='text/css' href='"\
+            + website->font["link"].get<std::string>() + "'>"
+            );
+
+    website->generatePages();
+    website->generateCSS();
+
     return 0;
 }
