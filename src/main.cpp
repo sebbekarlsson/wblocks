e7abbdcbbc88ee2b339bee677f6e471138883318
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-08-10 20:10:22 +0200

    added "install" command

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 
 void printAvailableCommands(std::map<std::string, int> commands) {
+    std::cout << "Available commands:" << std::endl;
     for(auto const &ent1 : commands) {
-        std::cout << "Available commands:" << std::endl;
         std::cout << "- " << ent1.first << std::endl;
     }
 }
@@ -12,6 +12,7 @@ void printAvailableCommands(std::map<std::string, int> commands) {
 int main(int argc, char* argv[]) {
     std::map<std::string, int> commands;
     commands["compile"] = 1;
+    commands["install"] = 2;
 
     if(argc < 2) {
         std::cout << "No command was specified" << std::endl;
@@ -22,15 +23,26 @@ int main(int argc, char* argv[]) {
 
     switch (commands[argv[1]]) {
         case 1:
-            if (argc < 3) {
-                std::cout << "No directory was specified" << std::endl;
-                std::cout << "Usage: websitecompiler compile <directory>" << std::endl;
+            {
+                if (argc < 3) {
+                    std::cout << "No directory was specified" << std::endl;
+                    std::cout << "Usage: websitecompiler compile <directory>" << std::endl;
+                    return 0;
+                }
+
+                Website * website = new Website(std::string(argv[2]));
+                website->compile();
                 return 0;
             }
-
-            Website * website = new Website(std::string(argv[2]));
-            website->compile();
-            return 0;
+        break;
+        case 2:
+            {
+                if (argc < 3) {
+                    std::cout << "No module was specified" << std::endl;
+                    std::cout << "Usage: websitecompiler install <module>" << std::endl;
+                }
+                return 0;
+            }
         break;
     }
 
