b22fad0cf3bbe5d3851cae2c3458d58c5982c636
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-08-10 19:56:39 +0200

    The program now takes commands as an argument, added some helping messages

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,39 @@
 #include <string>
 
 
-int main(int argc, char* argv[]) {
-    Website * website = new Website(std::string(argv[1]));
-    
-    website->compile();
+void printAvailableCommands(std::map<std::string, int> commands) {
+    for(auto const &ent1 : commands) {
+        std::cout << "Available commands:" << std::endl;
+        std::cout << "- " << ent1.first << std::endl;
+    }
+}
 
+int main(int argc, char* argv[]) {
+
+    std::map<std::string, int> commands;
+    commands["compile"] = 1;
+
+    if(argc < 2) {
+        std::cout << "No command was specified" << std::endl;
+        std::cout << "Usage: websitecompiler <command>\n" << std::endl;
+        printAvailableCommands(commands);
+        return 0;
+    }
+
+    switch (commands[argv[1]]) {
+        case 1:
+            if (argc < 3) {
+                std::cout << "No directory was specified" << std::endl;
+                std::cout << "Usage: websitecompiler compile <directory>" << std::endl;
+                return 0;
+            }
+
+            Website * website = new Website(std::string(argv[2]));
+            website->compile();
+            return 0;
+        break;
+    }
+
+    std::cout << "Command not found" << std::endl;
     return 0;
 }
