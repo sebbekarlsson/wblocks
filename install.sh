5e415aae9f88f7fc3b7726b6d83a7da79b440ac0
Sebastian Karlsson <sebbekarlsson97@gmail.com>
2016-08-11 16:49:19 +0200

    Added stdout information to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
+echo "Compiling..."
 make
+
+echo "Moving binary..."
 cp ./websitecompiler /usr/local/bin/.
+
+echo "Creating websitecompiler directory"
+rm -rf /etc/websitecompiler
 mkdir /etc/websitecompiler
+
+echo "Moving necessary files to directory"
 cp src/shards /etc/websitecompiler/. -r
+
+echo "Done"
