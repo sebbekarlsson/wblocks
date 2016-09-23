65edd1c4fa179b361f39bcbd5c0d056e61b7a60b
Sebastian Karlsson <sebastian.karlsson@knowit.se>
2016-09-23 11:50:48 +0200

    code cleanup

--- src/Website.cpp
+++ src/Website.cpp
@@ -5,9 +5,15 @@ Website::Website(std::string directory) {
     this->dir = directory;
 
     ResourceManager::load(this->dir + "/site.json");
-    this->site = nlohmann::json::parse(ResourceManager::get(this->dir + "/site.json"));
+    this->site = nlohmann::json::parse(
+            ResourceManager::get(this->dir + "/site.json")
+            );
 
-    std::cout << "[Initializing]: Creating directory: " << this->site["title"].get<std::string>() << std::endl;
+    std::cout <<
+        "[Initializing]: Creating directory: " <<
+        this->site["title"].get<std::string>() <<
+        std::endl;
+
     mkdir(
             this->site["title"].get<std::string>().c_str(),
             S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH
@@ -17,15 +23,21 @@ Website::Website(std::string directory) {
             S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH
          );
 
-    /* move files from images to new images location */
+    // move files from images to new images location
     std::cout << "[Initializing]: Moving images: " << std::endl;
     DIR *dpdf;
     struct dirent *epdf;
     dpdf = opendir((this->dir + "/images").c_str());
     if (dpdf != NULL){
         while (epdf = readdir(dpdf)){
-            std::ifstream  src(this->dir + "/images/" + epdf->d_name, std::ios::binary);
-            std::ofstream  dst(this->site["title"].get<std::string>() + "/images/" + epdf->d_name,   std::ios::binary);
+            std::ifstream src(
+                    this->dir + "/images/" + epdf->d_name, std::ios::binary
+                    );
+            std::ofstream dst(
+                    this->site["title"].get<std::string>() +
+                    "/images/" + epdf->d_name,
+                    std::ios::binary
+                    );
             dst << src.rdbuf();
         }
     }
@@ -33,21 +45,30 @@ Website::Website(std::string directory) {
 
     ResourceManager::load("/etc/websitecompiler/shards/html.html");
     ResourceManager::load("/etc/websitecompiler/shards/style.css");
-    ResourceManager::load(this->dir + "/" + this->site["font"].get<std::string>());
+    ResourceManager::load(
+            this->dir + "/" + this->site["font"].get<std::string>()
+            );
 
     this->title = this->site["title"].get<std::string>(); 
     this->html = ResourceManager::get("/etc/websitecompiler/shards/html.html");
     this->css = ResourceManager::get("/etc/websitecompiler/shards/style.css");
-    this->font = nlohmann::json::parse(ResourceManager::get(this->dir + "/" + this->site["font"].get<std::string>()));
+    this->font = nlohmann::json::parse(
+                ResourceManager::get(
+                    this->dir + "/" + this->site["font"].get<std::string>()
+                )
+            );
 
     std::vector<std::string> _pages = this->site["pages"].get<std::vector<std::string>>();
     for(auto i : _pages) {
         if (!ResourceManager::fileExists(this->dir + "/" + i)) { continue; }
+
         ResourceManager::load(this->dir + "/" + i);
+
         if (
                 ResourceManager::get(this->dir + "/" + i).empty() ||
                 ResourceManager::get(this->dir + "/" + i) == ""
            ) { continue; }
+
         this->pages[
             this->replace_word(i, ".json", "")
         ] = ResourceManager::get(this->dir + "/" + i);
@@ -76,15 +97,27 @@ void Website::addHeaderElement(std::string text) {
  * @param String fallbackFamily
  * @param String link
  */
-void Website::setFont(std::string family, std::string fallbackFamily, std::string link) {
+void Website::setFont(
+        std::string family,
+        std::string fallbackFamily,
+        std::string link
+        ) {
     this->family = family;
     this->fallbackFamily = fallbackFamily;
     this->addHeaderElement(
             "<link rel='stylesheet' type='text/css' href='"\
             + link + "'>"
             );
-    this->css = this->replace_word(this->css, "{{family}}", this->family);
-    this->css = this->replace_word(this->css, "{{fallback-family}}", this->fallbackFamily);
+    this->css = this->replace_word(
+            this->css,
+            "{{family}}",
+            this->family
+            );
+    this->css = this->replace_word(
+            this->css,
+            "{{fallback-family}}",
+            this->fallbackFamily
+            );
 }
 
 /**
@@ -93,7 +126,10 @@ void Website::setFont(std::string family, std::string fallbackFamily, std::strin
  * @param String title
  * @param String content
  */
-bool Website::addPage(std::string title, std::string content) {
+bool Website::addPage(
+        std::string title,
+        std::string content
+        ) {
     this->pages.insert(std::pair<std::string, std::string>(title, content));
 }
 
@@ -124,15 +160,29 @@ void Website::generatePages() {
         for(auto i : page_json["sections"]) {
             for (auto ii : i["children"]) {
                 if (ii["type"].get<std::string>() == "module") {
-                    std::string module_html_path = this->dir + "/" + ii["source"].get<std::string>() + "/" + "module.html";
-                    std::string module_css_path = this->dir + "/" + ii["source"].get<std::string>() + "/" + "module.css";
+                    std::string module_html_path = this->dir +
+                        "/" +
+                        ii["source"].get<std::string>() +
+                        "/" +
+                        "module.html";
+                    std::string module_css_path = this->dir +
+                        "/" +
+                        ii["source"].get<std::string>() +
+                        "/" +
+                        "module.css";
 
                     ResourceManager::load(module_html_path);
                     ResourceManager::load(module_css_path);
 
-                    std::string module_html = ResourceManager::get(module_html_path);
-                    std::string module_css = ResourceManager::get(module_css_path);
+                    std::string module_html = ResourceManager::get(
+                            module_html_path
+                            );
+                    std::string module_css = ResourceManager::get(
+                            module_css_path
+                            );
 
+                    // Temporary solution for parsing and applying jinja
+                    // variables.
                     std::smatch sm;
                     if (regex_search(module_html, sm, r)){
                         for (int i=1; i<sm.size(); i++){
@@ -142,26 +192,44 @@ void Website::generatePages() {
                                     if (v.find("{{") != std::string::npos) {
                                         v = this->replace_word(v, "{{", "");
                                         v = this->replace_word(v, "}}", "");
-                                        ResourceManager::load(this->dir + "/" + v);
-                                        v = ResourceManager::get(this->dir + "/" + v);
+                                        ResourceManager::load(
+                                                this->dir + "/" + v
+                                                );
+                                        v = ResourceManager::get(
+                                                this->dir + "/" + v
+                                                );
                                     }
 
-                                    std::cout << "[Generating Page]: " << ent1.first << " " << it.key() << ": " << v << std::endl;
-                                    module_html = this->replace_word(module_html, "{{"+it.key()+"}}", v);
+                                    std::cout << "[Generating Page]: " <<
+                                        ent1.first <<
+                                        " " <<
+                                        it.key() <<
+                                        ": " <<
+                                        v <<
+                                        std::endl;
+
+                                    module_html = this->replace_word(
+                                            module_html,
+                                            "{{"+it.key()+"}}",
+                                            v
+                                            );
                                 }
                             }
                         }
                     }
 
                     this->css += "\n" + module_css;
-
                     page_content += "<section>\n" + module_html + "</section>\n";
                 }
             }
         }
 
         ResourceManager::write_new(this->title + "/" + ent1.first + ".html",
-                this->replace_word(this->formatHTML(this->html), "{{body}}", page_content)
+                this->replace_word(
+                    this->formatHTML(this->html),
+                    "{{body}}",
+                    page_content
+                    )
                 );
     }
 }
@@ -193,7 +261,11 @@ std::string Website::formatHTML(std::string _html) {
 /**
  * Replace a word in a String with another word
  */
-std::string Website::replace_word(std::string text, std::string word, std::string replacement) {
+std::string Website::replace_word(
+        std::string text,
+        std::string word,
+        std::string replacement
+        ) {
     size_t start_pos = text.find(word);
     if(start_pos == std::string::npos)
         return text;
